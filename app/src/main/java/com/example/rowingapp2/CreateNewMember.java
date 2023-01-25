8800f2127421b5f878dc40ce0787aca6bae8d8b4
jadel <jade.libson@gmail.com>
2023-01-25 16:30:15 -0600

    commit w/ buttons linked up and variables added to members page (gender & side) and added methods for aquiring data from CreateNewMember page

--- app/src/main/java/com/example/rowingapp2/CreateNewMember.java
+++ app/src/main/java/com/example/rowingapp2/CreateNewMember.java
@@ -3,6 +3,9 @@ package com.example.rowingapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class CreateNewMember extends AppCompatActivity {
 
@@ -13,10 +16,39 @@ public class CreateNewMember extends AppCompatActivity {
     }
 
     //member info to use on creation
-    String name;
+    String fName;
+    String lName;
     int age;
-    boolean isFemale;
+    boolean isFemale = true;
     boolean isPort;
     boolean isStarboard;
+
+    public void getFName(View view)
+    {
+        EditText editFName = (EditText) findViewById(R.id.editfName);
+        fName = editFName.getText().toString();
+    }
+    public void getLName(View view)
+    {
+        EditText editLName = (EditText) findViewById(R.id.editlName);
+        lName = editLName.getText().toString();
+    }
+    public void isPort(View view) {isPort = !isPort;}
+    public void isStarboard(View view) {isStarboard = !isStarboard;}
+    public void isFemale(View view)                                 //NOTE: Look up proper syntax for radio buttons & groups
+    {
+        RadioButton fem = (RadioButton) findViewById(R.id.female);
+        RadioButton mal = (RadioButton) findViewById(R.id.male);
+        if(fem.isChecked())
+        {
+            isFemale = true;
+        }
+        else if(mal.isChecked())
+        {
+            isFemale = false;
+        }
+    }
+
+
 
 }
